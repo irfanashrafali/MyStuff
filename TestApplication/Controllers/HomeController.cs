048eb904b8b2775cb732c7e936496344579f6fbd
Irfan <irfanashrafali@gmail.com>
2016-08-08 18:19:41 +1200

    Add Text

--- TestApplication/Controllers/HomeController.cs
+++ TestApplication/Controllers/HomeController.cs
@@ -15,7 +15,7 @@ namespace TestApplication.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description page. My Message.";
 
             return View();
         }
